eddeed786564999a6941783a995adbb594403741
MiguelCruzBPC <109745935+MiguelCruzBPC@users.noreply.github.com>
2025-09-17 17:08:28 -0500

    code updated with Nish during last meeting talking about this topic.

--- src/FileCabinet/SuiteScripts/BPC/Intercompany Drop Shipment/[BPC]_create_so_po_inter_ue.js
+++ src/FileCabinet/SuiteScripts/BPC/Intercompany Drop Shipment/[BPC]_create_so_po_inter_ue.js
@@ -7,6 +7,57 @@ define(['N/record', 'N/runtime'], /**
  * @param{runtime} runtime
  */ (record, runtime) => {
   /**
+   * Defines the function definition that is executed before record is submitted.
+   * @param {Object} scriptContext
+   * @param {Record} scriptContext.newRecord - New record
+   * @param {Record} scriptContext.oldRecord - Old record
+   * @param {string} scriptContext.type - Trigger type; use values from the context.UserEventType enum
+   * @since 2015.2
+   */
+  const beforeSubmit = (scriptContext) => {
+    try {
+      // Intercompany Sales Order Process
+      salesOrderProcess(scriptContext);
+    } catch (e) {
+      log.error({
+        title: 'Error beforeSubmit',
+        details: e
+      });
+    }
+  };
+
+  const salesOrderProcess = (scriptContext) => {
+    if (scriptContext.type !== scriptContext.UserEventType.CREATE) {
+      return;
+    }
+
+    // Get the value of internal id for approve the purchase order
+    const purchaseOrdApproveStatus = runtime
+      .getCurrentScript()
+      .getParameter({ name: 'custscript_bpc_approval_status' });
+    if (purchaseOrdApproveStatus) {
+      const purchOrdRec = scriptContext.newRecord;
+
+      // Get the intercompany status in the purchase order
+      const interStatus = purchOrdRec.getValue({
+        fieldId: 'approvalstatus'
+      });
+      log.debug({
+        title: 'interStatus',
+        details: interStatus
+      });
+      // ONLY UPDATE THE APPROVED STATUS IF THE TRANSACTION IS A INTERCOMPANY TRANS
+      if (interStatus) {
+        // Set the value into the record
+        purchOrdRec.setValue({
+          fieldId: 'approvalstatus',
+          value: purchaseOrdApproveStatus
+        });
+      }
+    }
+  };
+
+  /**
    * Defines the function definition that is executed after record is submitted.
    * @param {Object} scriptContext
    * @param {Record} scriptContext.newRecord - New record
@@ -19,10 +70,10 @@ define(['N/record', 'N/runtime'], /**
       // Get the purchase order ID
       const poID = scriptContext.newRecord.id;
 
-      // get the sales order rec
+      // get the purchase order rec
       const interPurchaseOrder = scriptContext.newRecord;
       if (poID) {
-        // Get the triggered sales order record
+        // Get the triggered sales order record data
         const salesOrdCustomerData = getSOTriggerData(interPurchaseOrder);
         log.debug({
           title: 'salesOrdCustomerData',
@@ -31,32 +82,12 @@ define(['N/record', 'N/runtime'], /**
 
         // Validate if the customer sales order has data for the intercompany creation
         if (salesOrdCustomerData && salesOrdCustomerData.arrayLineItem.length) {
-          // Get the value of Paired internal ID from script parameter
-          const intercompanyPairedStatus = runtime
-            .getCurrentScript()
-            .getParameter({ name: 'custscript_bpc_inter_status_paired' });
-
           // Create the sales order intercompany
-          const soIdCreated = createInterTransaction(
-            salesOrdCustomerData,
-            intercompanyPairedStatus,
-            poID
-          );
+          const soIdCreated = createInterTransaction(salesOrdCustomerData, poID);
           if (soIdCreated) {
-            // Get the approval status from parameters to update the intercompany purchase order
-            const approvalStatusApproved = runtime
-              .getCurrentScript()
-              .getParameter({ name: 'custscript_bpc_approval_status' });
-
-            // Update the intercompany purchase order
-            record.submitFields({
-              type: record.Type.PURCHASE_ORDER,
-              id: poID,
-              values: {
-                intercotransaction: soIdCreated,
-                intercostatus: intercompanyPairedStatus,
-                approvalstatus: approvalStatusApproved
-              }
+            log.debug({
+              title: 'soIdCreated',
+              details: soIdCreated
             });
           }
         }
@@ -70,39 +101,54 @@ define(['N/record', 'N/runtime'], /**
   };
 
   /** This function allow the creation of sales order and purchase order intercompany transactions* */
-  const createInterTransaction = (customerSalesOrderData, intercompanyPairedStatus, poID) => {
-    // Create the Sales Order
-    const soRecord = record.create({
-      type: record.Type.SALES_ORDER,
-      isDynamic: true
-    });
-
+  const createInterTransaction = (customerSalesOrderData, poID) => {
     // Set main body fields
     // hardcode customer - IC-Shaver Industries, LLC
     const interSOcustomerId = runtime
       .getCurrentScript()
       .getParameter({ name: 'custscript_bpc_inter_so_customer' });
 
+    // harcode subsidiary - Shaver Industries Inc
+    const interSOsubsidiaryId = runtime
+      .getCurrentScript()
+      .getParameter({ name: 'custscript_bpc_inter_so_subsidiary' });
+
+    // hardcode currency - Canadian Dollar CAD
+    const interSOCadId = runtime
+      .getCurrentScript()
+      .getParameter({ name: 'custscript_bpc_inter_co_currency' });
+
+    // Create the Sales Order
+    const soRecord = record.create({
+      type: record.Type.SALES_ORDER,
+      isDynamic: true,
+      defaultValues: {
+        ictran: 8639,
+        entity: interSOcustomerId, // Intercompany vendor,
+        // otherrefnum: customerSalesOrderData.poNumber,
+        subsidiary: interSOsubsidiaryId,
+        // department: customerSalesOrderData.department,
+        currency: interSOCadId
+      }
+    });
+
+    /* // Customer set
     if (interSOcustomerId) {
+      log.debug({
+        title: 'interSOcustomerId',
+        details: interSOcustomerId
+      });
       soRecord.setValue({
         fieldId: 'entity',
         value: interSOcustomerId
       });
-    }
+    } */
 
-    if (intercompanyPairedStatus) {
-      // Set the intercompany status as Paired
+    if (customerSalesOrderData.poNumber) {
+      // PO Number
       soRecord.setValue({
-        fieldId: 'intercostatus',
-        value: intercompanyPairedStatus // Paired
-      });
-    }
-
-    // Set the Paired Intercompany Transaction field in the PURCHASE ORDER
-    if (poID) {
-      soRecord.setValue({
-        fieldId: 'intercotransaction',
-        value: poID
+        fieldId: 'otherrefnum',
+        value: customerSalesOrderData.poNumber
       });
     }
 
@@ -112,23 +158,53 @@ define(['N/record', 'N/runtime'], /**
       value: new Date()
     });
 
-    // Lcoation
-    soRecord.setValue({
-      fieldId: 'location',
-      value: customerSalesOrderData.location
-    });
+    /* Subsidiary from parameters
+    if (interSOsubsidiaryId) {
+      log.debug({
+        title: 'interSOsubsidiaryId',
+        details: interSOsubsidiaryId
+      });
+      soRecord.setValue({
+        fieldId: 'subsidiary',
+        value: interSOsubsidiaryId
+      });
+    } */
 
-    // Department
-    soRecord.setValue({
-      fieldId: 'department',
-      value: customerSalesOrderData.department
-    });
+    if (customerSalesOrderData.department) {
+      // Department
+      soRecord.setValue({
+        fieldId: 'department',
+        value: customerSalesOrderData.department
+      });
+    }
 
-    // PO Number
-    soRecord.setValue({
-      fieldId: 'otherrefnum',
-      value: customerSalesOrderData.poNumber
-    });
+    if (customerSalesOrderData.location) {
+      // Lcoation
+      soRecord.setValue({
+        fieldId: 'location',
+        value: customerSalesOrderData.location
+      });
+    }
+
+    /* Set the currency as Canadian Dollar CAD
+    if (interSOCadId) {
+      log.debug({
+        title: 'interSOCadId',
+        details: interSOCadId
+      });
+      soRecord.setValue({
+        fieldId: 'currency',
+        value: interSOCadId
+      });
+    }
+
+    // Set the intercompany Purchase Order
+    if (poID) {
+      soRecord.setValue({
+        fieldId: 'intercotransaction',
+        value: poID
+      });
+    } */
 
     // Create the lines from the trigger sales order
     if (customerSalesOrderData.arrayLineItem.length > 0) {
@@ -177,28 +253,25 @@ define(['N/record', 'N/runtime'], /**
     }
 
     // Save the Sales Order
-    const transactionID = soRecord.save();
+    const transactionID = soRecord.save({
+      enableSourcing: true,
+      ignoreMandatoryFields: true
+    });
     log.debug({
       title: 'Intercompany sales Order Created',
       details: transactionID
     });
+
     return transactionID;
   };
 
   const getSOTriggerData = (interPurchaseOrder) => {
+    // Get the created from sales order = Customer Sales Order
     const createdfromSO = interPurchaseOrder.getValue({
       fieldId: 'createdfrom'
     });
 
-    // Get the value of intercompany status pending
-    const intercompanyStatus = runtime
-      .getCurrentScript()
-      .getParameter({ name: 'custscript_bpc_inter_status_pending' });
-    // Get the current intercompany status
-    const poInterStatus = interPurchaseOrder.getValue({
-      fieldId: 'intercostatus'
-    });
-    if (createdfromSO && Number(intercompanyStatus) === Number(poInterStatus)) {
+    if (createdfromSO) {
       // Load the customer sales order
       const triggerSORec = record.load({
         type: record.Type.SALES_ORDER,
@@ -285,5 +358,5 @@ define(['N/record', 'N/runtime'], /**
     return false;
   };
 
-  return { afterSubmit };
+  return { beforeSubmit, afterSubmit };
 });
