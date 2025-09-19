262d236206dbd34b087f92170b0ee86d79eb82e5
MiguelCruzBPC <109745935+MiguelCruzBPC@users.noreply.github.com>
2025-09-19 13:53:35 -0500

    Code completed to allow the creation of the advanced intercompany journal entry creation

--- src/FileCabinet/SuiteScripts/BPC/Intercompany Drop Shipment/[BPC]_create_advanced_journal_ue.js
+++ src/FileCabinet/SuiteScripts/BPC/Intercompany Drop Shipment/[BPC]_create_advanced_journal_ue.js
@@ -2,10 +2,11 @@
  * @NApiVersion 2.1
  * @NScriptType UserEventScript
  */
-define(['N/record', 'N/search'], /**
+define(['N/record', 'N/search', 'N/runtime'], /**
  * @param{record} record
  * @param{search} search
- */ (record, search) => {
+ * @param{runtime} runtime
+ */ (record, search, runtime) => {
   /**
    * Defines the function definition that is executed after record is submitted.
    * @param {Object} scriptContext
@@ -16,14 +17,17 @@ define(['N/record', 'N/search'], /**
    */
   const afterSubmit = (scriptContext) => {
     try {
+      // Run only on creation
+      if (scriptContext.type !== scriptContext.UserEventType.CREATE) {
+        return;
+      }
+
       // Get the sales order record
       const soRecord = scriptContext.newRecord;
+      // Get the sales order ID
+      const soID = scriptContext.newRecord.id;
       // Get the sales order information
-      const { arrayItemIds, arrayLineData } = getSalesOrderData(soRecord);
-      log.debug({
-        title: 'arrayLineData',
-        details: arrayLineData
-      });
+      const { arrayItemIds, arrayLineData, headerDataSO } = getSalesOrderData(soRecord);
       if (arrayItemIds.length > 0) {
         const itemVendorRates = getAllResultsPaged(getItemVendorRate(arrayItemIds));
 
@@ -36,7 +40,21 @@ define(['N/record', 'N/search'], /**
 
         if (Number(totalAmounSO) > 0) {
           // Create the Advanced Intercompany Journal Entry
-          createJournalEntry(arrayLineData, itemVendorRates);
+          const advInterJournalEntryID = createJournalEntry(totalAmounSO, headerDataSO.subsidiary);
+          log.debug({
+            title: 'advInterJournalEntryID',
+            details: advInterJournalEntryID
+          });
+          if (advInterJournalEntryID && soID) {
+            // Submit the advanced intercompany JE in the sales order
+            record.submitFields({
+              type: record.Type.SALES_ORDER,
+              id: soID,
+              values: {
+                custbody_bpc_adv_inter_je: advInterJournalEntryID
+              }
+            });
+          }
         }
       }
     } catch (e) {
@@ -47,17 +65,137 @@ define(['N/record', 'N/search'], /**
     }
   };
 
-  const createJournalEntry = (totalAmounSO) => {};
+  const createJournalEntry = (totalAmounSO, subsidiary) => {
+    const journalRec = record.create({
+      type: record.Type.ADV_INTER_COMPANY_JOURNAL_ENTRY,
+      isDynamic: true
+    });
+
+    // Set the subsidiary
+    journalRec.setValue({
+      fieldId: 'subsidiary',
+      value: subsidiary
+    });
+
+    // set the approval status
+    journalRec.setValue({
+      fieldId: 'approvalstatus',
+      value: 2 // Approved
+    });
+
+    // Get Script Parameters (accounts)
+    const scriptObj = runtime.getCurrentScript();
+    const accInterPayables = scriptObj.getParameter({ name: 'custscript_bpc_acc_inter_payables' });
+    const accInterCOGS = scriptObj.getParameter({ name: 'custscript_bpc_acc_inter_cogs' });
+    const accInterReceivables = scriptObj.getParameter({
+      name: 'custscript_bpc_acc_inter_receivables'
+    });
+    const accInterIncome = scriptObj.getParameter({ name: 'custscript_bpc_acc_inter_income' });
+    const interVendorInc = scriptObj.getParameter({ name: 'custscript_bpc_inter_vendor' }); //	IC-Shaver Industries Inc
+    const interVendorLLC = scriptObj.getParameter({ name: 'custscript_bpc_vendor_llc' }); //	IC-Shaver Industries, LLC
+    const subsidiaryLLC = scriptObj.getParameter({ name: 'custscript_bpc_subsidiary_llc' }); // Shaver Industries, LLC
+    const subsidiaryInc = scriptObj.getParameter({ name: 'custscript_bpc_subsidiary_inc' }); // Shaver Industries Inc
+
+    // Debit line
+    addLineJournal(
+      journalRec,
+      'debit',
+      accInterPayables,
+      totalAmounSO,
+      subsidiaryLLC,
+      subsidiaryInc,
+      interVendorInc
+    );
+    // Credit line
+    addLineJournal(journalRec, 'credit', accInterCOGS, totalAmounSO, subsidiaryLLC, subsidiaryInc);
+    // Debit line
+    addLineJournal(
+      journalRec,
+      'debit',
+      accInterReceivables,
+      totalAmounSO,
+      subsidiaryInc,
+      subsidiaryLLC,
+      interVendorLLC
+    );
+    // Credit line
+    addLineJournal(
+      journalRec,
+      'credit',
+      accInterIncome,
+      totalAmounSO,
+      subsidiaryInc,
+      subsidiaryLLC
+    );
+
+    return journalRec.save();
+  };
+
+  const addLineJournal = (
+    journalRec,
+    typeLine,
+    accParam,
+    soAmount,
+    subsidiary,
+    dueToSubsidiary,
+    vendorId
+  ) => {
+    // ADD the new line DEBIT OR CREDIT
+    journalRec.selectNewLine({
+      sublistId: 'line'
+    });
+    // Subsidiary
+    if (subsidiary) {
+      journalRec.setCurrentSublistValue({
+        sublistId: 'line',
+        fieldId: 'linesubsidiary',
+        value: subsidiary
+      });
+    }
+
+    // account
+    if (accParam) {
+      journalRec.setCurrentSublistValue({
+        sublistId: 'line',
+        fieldId: 'account',
+        value: accParam
+      });
+    }
+
+    // amount
+    if (soAmount) {
+      journalRec.setCurrentSublistValue({
+        sublistId: 'line',
+        fieldId: typeLine,
+        value: soAmount
+      });
+    }
+
+    // set Entity
+    if (vendorId) {
+      journalRec.setCurrentSublistValue({
+        sublistId: 'line',
+        fieldId: 'entity',
+        value: vendorId
+      });
+    }
+
+    // Set the Due To Subsidiary
+    if (dueToSubsidiary) {
+      journalRec.setCurrentSublistValue({
+        sublistId: 'line',
+        fieldId: 'duetofromsubsidiary',
+        value: dueToSubsidiary
+      });
+    }
+
+    // commit the line
+    journalRec.commitLine({
+      sublistId: 'line'
+    });
+  };
 
   const getTotalVendorAmountSO = (itemVendorRates, arrayLineData) => {
-    log.debug({
-      title: 'itemVendorRates',
-      details: itemVendorRates
-    });
-    log.debug({
-      title: 'arrayLineData',
-      details: arrayLineData
-    });
     // Build a map { itemId: vendorCost }
     const vendorCostMap = itemVendorRates.reduce((acc, item) => {
       acc[item.itemId] = Number(item.vendorCost);
@@ -111,6 +249,10 @@ define(['N/record', 'N/search'], /**
   }
 
   const getSalesOrderData = (soRecord) => {
+    // Get the header data
+    const headerDataSO = {
+      subsidiary: soRecord.getValue({ fieldId: 'subsidiary' })
+    };
     const soLineCount = soRecord.getLineCount({
       sublistId: 'item'
     });
@@ -168,7 +310,8 @@ define(['N/record', 'N/search'], /**
 
     return {
       arrayItemIds,
-      arrayLineData
+      arrayLineData,
+      headerDataSO
     };
   };
 
